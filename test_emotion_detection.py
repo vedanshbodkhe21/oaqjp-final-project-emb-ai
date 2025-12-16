47d2c9107d3dc8e8d7520f677aed301114618255
Vedansh Bodkhe <vedansh.bodkhe.21@gmail.com>
2025-12-16 06:52:44 -0500

    test: refactor existing tests

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -9,19 +9,30 @@ class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detection(self):
         """Test emotion detection capabilities of the package"""
 
-        tests = [
-            ('I am glad this happened', 'joy'),
-            ('I am really mad about this', 'anger'),
-            ('I feel disgusted just hearing about this', 'disgust'),
-            ('I am so sad about this', 'sadness'),
-            ('I am really afraid that this will happen', 'fear')
-        ]
+        # Run tests
+        dominant_emotion = emotion_detector(
+            'I am glad this happened'
+            )['dominant_emotion']
+        self.assertEqual(dominant_emotion, 'joy')
 
-        for item in tests:
-            # Find dominant emotion of the current test case
-            dominant_emotion = emotion_detector(item[0])['dominant_emotion']
+        dominant_emotion = emotion_detector(
+            'I am really mad about this'
+            )['dominant_emotion']
+        self.assertEqual(dominant_emotion, 'anger')
 
-            # Assert it's equal the expected emotion
-            self.assertEqual(dominant_emotion, item[1])
+        dominant_emotion = emotion_detector(
+            'I feel disgusted just hearing about this'
+            )['dominant_emotion']
+        self.assertEqual(dominant_emotion, 'disgust')
+
+        dominant_emotion = emotion_detector(
+            'I am so sad about this'
+            )['dominant_emotion']
+        self.assertEqual(dominant_emotion, 'sadness')
+
+        dominant_emotion = emotion_detector(
+            'I am really afraid that this will happen'
+            )['dominant_emotion']
+        self.assertEqual(dominant_emotion, 'fear')
 
 unittest.main()
